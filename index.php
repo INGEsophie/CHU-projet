36cb71b0392ce7d2ba6b1c65ae8ba331e1c3bc32
Ukeru <alexisbrean@gmail.com>
2017-03-06 11:40:09 +0100

    en cours

--- index.php
+++ index.php
@@ -1,21 +1,146 @@
 <!Doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>PROJET CHU</title>
+<html="fr">
+<head>
+  <meta charset="utf-8">
+  <title>PROJET CHU</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
-  </head>
+</head>
 
-  <body>
-    
-  </body>
+<body>
 
-  <!-- Script -->
-  <script src="bootstrap/js/bootsrapt.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <!-- Header -->
+  <?php
+  include("header.html");
+  ?>
+
+  <!-- Vigniettes -->
+  <section class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Pédiatrie
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Urgence
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="thumbnail">
+              <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+              <div class="caption">
+                <h3>
+                  Maternité
+                </h3>
+                <p>
+                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+                <p>
+                  <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="thumbnail">
+                      <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+                      <div class="caption">
+                        <h3>
+                          Consultation génèrale
+                        </h3>
+                        <p>
+                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                        </p>
+                        <p>
+                          <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="thumbnail">
+                      <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+                      <div class="caption">
+                        <h3>
+                          Radiologie
+                        </h3>
+                        <p>
+                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                        </p>
+                        <p>
+                          <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="thumbnail">
+                      <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+                      <div class="caption">
+                        <h3>
+                          Chiurgie
+                        </h3>
+                        <p>
+                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                        </p>
+                        <p>
+                          <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer -->
+  <?php
+  include("footer.html");
+  ?>
+</body>
+
+<!-- Script -->
+<script src="bootstrap/js/bootsrapt.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 </html>
