3473bc5abd25bfdde1386c9205f2cbed682a20a0
INGEsophie <sophie.nath.inge@gmail.com>
2017-03-06 15:16:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,88 +19,74 @@
 
   <!-- Vigniettes -->
   <section class="container-fluid">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="thumbnail">
-              <img class="image" alt="Bootstrap Thumbnail First" src="images/pediatre1.jpg" />
-              <div class="caption">
-                <h3>
-                  P&eacute;diatrie
-                </h3>
-                <p>
-                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="thumbnail">
-              <img class="image" alt="Bootstrap Thumbnail Second" src="images/urgence1.jpg" />
-              <div class="caption">
-                <h3>
-                  Urgence
-                </h3>
-                <p>
-                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="thumbnail">
-              <img class="image" alt="Bootstrap Thumbnail Third" src="images/maternite1.png" />
-              <div class="caption">
-                <h3>
-                  Maternit&eacute;
-                </h3>
-                <p>
-                  Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                </p>
-              </div>
-            </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="intro">
+          <h1>CHU EVREUX</h1>
+          <p>Bienvenue sur le site du CHU d'Evreux.</p>
+        </div>
+      </div>
+    </div>
+        
+    <div class="row">
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail First" src="images/pediatre1.jpg" />
+          <div class="caption">
+            <h3>P&eacute;diatrie</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
           </div>
         </div>
-                <div class="row">
-                  <div class="col-md-4">
-                    <div class="thumbnail">
-                      <img class="image" alt="Bootstrap Thumbnail First" src="images/consultation1.png" />
-                      <div class="caption">
-                        <h3>
-                          Consultation g&eacute;n&egrave;rale
-                        </h3>
-                        <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="thumbnail">
-                      <img class="image" alt="Bootstrap Thumbnail Second" src="images/radiologie1.jpg" />
-                      <div class="caption">
-                        <h3>
-                          Radiologie
-                        </h3>
-                        <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="thumbnail">
-                      <img class="image" alt="Bootstrap Thumbnail Third" src="images/chirurgie1.jpg" />
-                      <div class="caption">
-                        <h3>
-                          Chiurgie
-                        </h3>
-                        <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail Second" src="images/urgence1.jpg" />
+          <div class="caption">
+            <h3>Urgence</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail Third" src="images/maternite1.png" />
+          <div class="caption">
+            <h3>Maternit&eacute;</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail First" src="images/consultation1.png" />
+          <div class="caption">
+            <h3>Consultation g&eacute;n&egrave;rale</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail Second" src="images/radiologie1.jpg" />
+          <div class="caption">
+            <h3>Radiologie</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail">
+          <img class="image" alt="Bootstrap Thumbnail Third" src="images/chirurgie1.jpg" />
+          <div class="caption">
+            <h3>Chiurgie</h3>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
   <!-- footer -->
