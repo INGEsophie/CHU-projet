4a6d95620f75eef5239d61d363b53d34a4559557
philippe.coudoux <pcoudoux27@gmail.com>
2017-03-08 14:00:52 +0100

    modifs

--- traitement.php
+++ traitement.php
@@ -15,11 +15,11 @@
   <?php
   
   // On vérifie si la variable existe et sinon elle vaut NULL
-$nom = isset($_POST['Nom']) ? $_POST['Nom'] : NULL;
-$prenom = isset($_POST['Prenom']) ? $_POST['Prenom'] : NULL;
-$email = isset($_POST['Email']) ? $_POST['Email'] : NULL;
-$datenaissance = isset($_POST['DateNaissance']) ? $_POST['DateNaissance'] : NULL;
-$adressepostale = isset($_POST['AdressPostale']) ? $_POST['AdressPostale'] : NULL;
+$nom = isset($_POST['nom']) ? $_POST['nom'] : NULL;
+$prenom = isset($_POST['prenom']) ? $_POST['prenom'] : NULL;
+$email = isset($_POST['email']) ? $_POST['email'] : NULL;
+$datenaissance = isset($_POST['datenaissance']) ? $_POST['datenaissance'] : NULL;
+$adressepostale = isset($_POST['adresse']) ? $_POST['adresse'] : NULL;
 $numsecu = isset($_POST['NumSecu']) ? $_POST['NumSecu'] : NULL;
 
   
@@ -34,8 +34,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully"; 
-$sql = 'INSERT INTO patients (Nom, Prenom,DateNaissance, AdressPostale, NumSecu, Email) 
-VALUES("'.$nom.'", "'.$prenom.'", "'.$datenaissance.'" , "'.$adressepostale.'", "'.$numsecu.'", "'.$email.'")';
+$sql = 'INSERT INTO patients 
+VALUES("", "'.$nom.'", "'.$prenom.'", "'.$datenaissance.'" , "'.$adressepostale.'", "'.$numsecu.'", "'.$email.'")';
 if ($conn->query($sql) === TRUE) {
     echo "les données ont bien étés insérées dans la base de données";
 } else {
