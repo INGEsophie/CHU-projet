fecbec0071287c612a7d92d612b72c82863352e2
frejodo <frejodo@gmail.com>
2017-03-08 11:59:39 +0100

    N°6

--- formulaire.php
+++ formulaire.php
@@ -20,54 +20,34 @@
        <legend>Vos coordonnées</legend> <!-- Titre du fieldset --> 
 
        <label for="Nom">Quel est votre nom ?</label>
-       <input type="text" name="Nom" id="Nom" /><br><br>
+       <input type="text" name="nom" id="Nom" /><br><br>
 
        <label for="Prenom">Quel est votre prénom ?</label>
-       <input type="text" name="Prenom" id="Prenom" /><br><br>
+       <input type="text" name="prenom" id="Prenom" /><br><br>
  
        <label for="Email">Quel est votre e-mail ?</label>
-       <input type="email" name="Email" id="Email" /><br><br>
+       <input type="email" name="email" id="Email" /><br><br>
        
        <label for="DateNaissance">Quel est votre date de naissance ?</label>
-       <input type="date" name="DateNaissance" id="DateNaissance"><br><br>
+       <input type="date" name="datenaissance" id="DateNaissance"><br><br>
 
        <label for="AdressePostale">Votre adresse :</label>
             <input type="text" name="adresse" id="AdressePostale"  size="30" maxlength="80" /><br><br>
        
        <label for="NumSecu">Votre numero de sécurité social :</label>
-       <input type="text" name="NumSecu" id="NumSecu" size="15" minlength="15" maxlength="15" />
+       <input type="text" name="numsecu" id="NumSecu" size="15" minlength="15" maxlength="15" />
        
        
        
     </fieldset>
 
     
-        <fieldset>
-            <legend>Votre consultation</legend>
-            
-      <label for="DateConsul">Le jour de consultation</label>
-       <input type="date" name="dateConsul" id="DateConsul"><br><br>
-        
-     <label for="HeureConsul">L'heure de consultation</label>
-       <input type="time" name="HeureConsul" id="HeureConsul"><br><br>
-            
-     <label for="NomService">Le service :</label>
-            <input type="text" name="NomService" id="NomService"  size="15" maxlength="30" /><br><br>            
-            
-    <label for="ButConsul">Le but de la consultation :</label>
-            <textarea name="ButConsul" id="ButConsul" rows="4" cols="50"  ></textarea><br><br>
-            
        
-   
-      
-        </fieldset>
     
-<<<<<<< HEAD
+
     <input type="submit" name="envoyer" value="Envoyer" />
        
-=======
-    
->>>>>>> origin/frejodo
+
     </form>
 
 
--- traitement.php
+++ traitement.php
@@ -16,16 +16,12 @@
   <?php
   
   // On vérifie si la variable existe et sinon elle vaut NULL
-$Nom = isset($_POST['Nom']) ? $_POST['Nom'] : NULL;
-$Prenom = isset($_POST['Prenom']) ? $_POST['Prenom'] : NULL;
-$Email = isset($_POST['Email']) ? $_POST['Email'] : NULL;
-$DateNaissance = isset($_POST['DateNaissance']) ? $_POST['DateNaissance'] : NULL;
-$AdressePostale = isset($_POST['AdressePostale']) ? $_POST['AdressePostale'] : NULL;
-$NumSecu = isset($_POST['NumSecu']) ? $_POST['NumSecu'] : NULL;
-$DateConsul = isset($_POST['DateConsul']) ? $_POST['DateConsul'] : NULL;
-$HeureConsul = isset($_POST['HeureConsul']) ? $_POST['HeureConsul'] : NULL;
-$ButConsul($_POST['ButConsul']) ? $_POST['ButConsul'] : NULL;
-$NomService = isset($_POST['NonService']) ? $_POST['NomService'] : NULL;
+$nom = isset($_POST['Nom']) ? $_POST['Nom'] : NULL;
+$prenom = isset($_POST['Prenom']) ? $_POST['Prenom'] : NULL;
+$email = isset($_POST['Email']) ? $_POST['Email'] : NULL;
+$datenaissance = isset($_POST['DateNaissance']) ? $_POST['DateNaissance'] : NULL;
+$adressepostale = isset($_POST['AdressPostale']) ? $_POST['AdressPostale'] : NULL;
+$numsecu = isset($_POST['NumSecu']) ? $_POST['NumSecu'] : NULL;
 
   
 $servername = "localhost";
@@ -39,31 +35,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully"; 
-$sql = 'INSERT INTO patients (IdPatient, Nom, Prenom,DateNaissance, AdressePostale, Email, DateConsul, HeureConsul, NomService) 
-VALUES("", "'.$Nom.'", "'.$Prenom.'", "'.$DateNaissance.'", "'.$Email.'", "'.$AdressePostale.'", "'.$NumSecu.'", "'.$DateConsul.'", "'.$HeureConsul.'", "'.$ButCOnsul.'", "'.$NomService.'")';
+$sql = 'INSERT INTO patients (IdPatient, Nom, Prenom,DateNaissance, AdressPostale, Email) 
+VALUES("", "'.$nom.'", "'.$prenom.'", "'.$datenaissance.'", "'.$email.'", "'.$adressepostale.'", "'.$numsecu.'")';
 if ($conn->query($sql) === TRUE) {
     echo "les données ont bien étés insérées dans la base de données";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 $conn->close();  
-  
-  echo("<center>Le nom est: $Nom</center><br><br>");
-  echo("<center>Le prénom est: $Prenom</center><br><br>");
-  echo("<center>La date est: $Email</center><br><br>"); 
-  echo("<center>Le lieu de naissance est: $DateNaissance</center><br><br>");
-  echo("<center>L'adresse postale est: $AdressePostale</center><br><br>");   
-  echo("<center>Le code postal est: $NumSecu</center><br><br>"); 
-  echo("<center>L'email est: $DateConsul</center><br><br>"); 
-  echo("<center>Le site est: $HeureConsul</center><br><br>"); 
-  echo("<center>Le numéro de téléphone est: $ButConsul</center><br><br>"); 
-  echo("<center>Le semestre est: $NomService</center><br><br>"); 
+
+  echo("<center>Le nom est: $nom</center><br><br>");
+  echo("<center>Le prénom est: $prenom</center><br><br>");
+  echo("<center>La date est: $email</center><br><br>"); 
+  echo("<center>Le lieu de naissance est: $datenaissance</center><br><br>");
+  echo("<center>L'adresse postale est: $adressepostale</center><br><br>");   
+  echo("<center>Le code postal est: $numsecu</center><br><br>"); 
+ 
   
   
-  echo("<center>Les connaissances sont:</center><br>");
-  foreach((array)$connaissances as $valeur){
-  echo("<center>$valeur</center><br>");
-  }
+ 
   ?>
   <hr>
   
