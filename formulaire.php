cd88919d94a71dbf6d999345bc5852c2ee27f755
INGEsophie <sophie.nath.inge@gmail.com>
2017-03-08 11:13:44 +0100

    MEP Formulaire

--- formulaire.php
+++ formulaire.php
@@ -2,9 +2,9 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Centre Hospitalier Universitaire de Rouen</title>
-  <link rel="stylesheet" href="style.css">
+  <title>CHU Evreux - Enregistrement patient</title>
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -13,58 +13,49 @@
     include("header.html"); 
   ?>
 
+  <div class="container-fluid">
+    <h2>Formulaire d'hospitalisation</h2>
+    <form method="post" action="traitement.php" class="formulaire">
+      <fieldset>
+        <legend>Coordonnées</legend> <!-- Titre du fieldset --> 
 
-  <h2>Formulaire d'hospitalisation.</h2>
-  <form method="post" action="traitement.php">
-    <fieldset>
-       <legend>Vos coordonnées</legend> <!-- Titre du fieldset --> 
+        <label for="nom">Quel est votre nom ?</label>
+        <input type="text" name="nom" id="nom" /><br><br>
 
-       <label for="Nom">Quel est votre nom ?</label>
-       <input type="text" name="Nom" id="Nom" /><br><br>
-
-       <label for="Prenom">Quel est votre prénom ?</label>
-       <input type="text" name="Prenom" id="Prenom" /><br><br>
- 
-       <label for="Email">Quel est votre e-mail ?</label>
-       <input type="email" name="Email" id="Email" /><br><br>
-       
-       <label for="DateNaissance">Quel est votre date de naissance ?</label>
-       <input type="date" name="DateNaissance" id="DateNaissance"><br><br>
-
-       <label for="AdressePostale">Votre adresse :</label>
-            <input type="text" name="adresse" id="AdressePostale"  size="30" maxlength="80" /><br><br>
-       
-       <label for="NumSecu">Votre numero de sécurité social :</label>
-       <input type="text" name="NumSecu" id="NumSecu" size="15" minlength="15" maxlength="15" />
-       
-       
-       
-    </fieldset>
-
-    
-        <fieldset>
-            <legend>Votre consultation</legend>
-            
-      <label for="DateConsul">Le jour de consultation</label>
-       <input type="date" name="dateConsul" id="DateConsul"><br><br>
-        
-     <label for="HeureConsul">L'heure de consultation</label>
-       <input type="time" name="HeureConsul" id="HeureConsul"><br><br>
-            
-     <label for="NomService">Le service :</label>
-            <input type="text" name="NomService" id="NomService"  size="15" maxlength="30" /><br><br>            
-            
-    <label for="ButConsul">Le but de la consultation :</label>
-            <textarea name="ButConsul" id="ButConsul" rows="4" cols="50"  ></textarea><br><br>
-            
-       
+        <label for="prenom">Quel est votre prénom ?</label>
+        <input type="text" name="prenom" id="prenom" /><br><br>
    
-      
-        </fieldset>
-    
-    <input type="submit" name="envoyer" value="Envoyer" />
-       
+        <label for="email">Quel est votre e-mail ?</label>
+        <input type="email" name="email" id="email" /><br><br>
+         
+        <label for="email">Quel est votre date de naissance ?</label>
+        <input type="date" name="dateNaissance" id="dateNaissance"><br><br>
+
+        <label for="adresse">Votre adresse :</label>
+        <input type="text" name="adresse" id="adresse"  size="30" maxlength="80" /><br><br>
+         
+        <label for="nusocial">Votre numero de sécurité social :</label>
+        <input type="text" name="nusocial" id="nusocial" size="15" minlength="15" maxlength="15" />   
+      </fieldset>
+      <hr>
+      <fieldset>
+        <legend>Votre consultation</legend>
+              
+        <label for="DateConsul">Le jour de consultation</label>
+        <input type="date" name="dateConsul" id="dateConsul"><br><br>
+        
+        <label for="HeureConsul">L'heure de consultation</label>
+        <input type="time" name="heureConsul" id="heureConsul"><br><br>
+              
+        <label for="ButConsul">Le but de la consultation :</label>
+        <input type="text" name="butconsul" id="ButConsul"  size="30" maxlength="80" /><br><br>
+              
+        <label for="NomService">Le service :</label>
+        <input type="text" name="NomService" id="NomService"  size="15" maxlength="30" /><br><br>    
+      </fieldset>
+      <input type="submit" name="envoyer" value="Envoyer" class="btn bnt-defaut" />
     </form>
+  </div>
 
   <?php 
     include("footer.html");
