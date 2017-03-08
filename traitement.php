e6826c3f905be7e6ae7c9ed6308db32688852212
frejodo <frejodo@gmail.com>
2017-03-08 10:20:55 +0100

    traitement

--- traitement.php
+++ traitement.php
@@ -1,41 +1,33 @@
-<?php
 
-$base = mysql_connect ('localhost', 'root', '');
-mysql_select_db ('bdchu', $base) ;
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Résultat du formulaire</title>
+  </head>
+  
+  <body>
 
-
-
-
-
-
-
-
+  <header></header>
+  <h1>Récapitulatif de votre inscription</h1>
+  
+  <input type="button" width="100px" height="40px" name="valider" value=" Accès Administrateur" onclick="window.location.href='admin.php';">
+  <hr>
+  <?php
+  
+  // On vérifie si la variable existe et sinon elle vaut NULL
 $Nom = isset($_POST['Nom']) ? $_POST['Nom'] : NULL;
-
 $Prenom = isset($_POST['Prenom']) ? $_POST['Prenom'] : NULL;
-
 $Email = isset($_POST['Email']) ? $_POST['Email'] : NULL;
-
 $DateNaissance = isset($_POST['DateNaissance']) ? $_POST['DateNaissance'] : NULL;
-
 $AdressePostale = isset($_POST['AdressePostale']) ? $_POST['AdressePostale'] : NULL;
-
 $NumSecu = isset($_POST['NumSecu']) ? $_POST['NumSecu'] : NULL;
-
 $DateConsul = isset($_POST['DateConsul']) ? $_POST['DateConsul'] : NULL;
-
 $HeureConsul = isset($_POST['HeureConsul']) ? $_POST['HeureConsul'] : NULL;
+$ButConsul($_POST['ButConsul']) ? $_POST['ButConsul'] : NULL;
+$NomService = isset($_POST['NonService']) ? $_POST['NomService'] : NULL;
 
-$ButConsul = isset($_POST['ButConsul']) ? $_POST['ButConsul'] : NULL;
-
-$NomService = isset($_POST['NomService']) ? $_POST['NomService'] : NULL;
-
-
-
-   
-
-
-
+  
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -48,11 +40,35 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully"; 
 $sql = 'INSERT INTO patients (IdPatient, Nom, Prenom,DateNaissance, AdressePostale, Email, DateConsul, HeureConsul, NomService) 
-VALUES("", "'.$Nom.'", "'.$Prenom.'", "'.$Date!naissance.'", "'.$AdressePostale.'", "'.$NumSecu.'", "'.$DateConsul.'", "'.$HeureConsul.'", "'.$ButConsul.'", "'.$NonService.'")';
+VALUES("", "'.$Nom.'", "'.$Prenom.'", "'.$DateNaissance.'", "'.$Email.'", "'.$AdressePostale.'", "'.$NumSecu.'", "'.$DateConsul.'", "'.$HeureConsul.'", "'.$ButCOnsul.'", "'.$NomService.'")';
 if ($conn->query($sql) === TRUE) {
     echo "les données ont bien étés insérées dans la base de données";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
-?>
+$conn->close();  
+  
+  echo("<center>Le nom est: $Nom</center><br><br>");
+  echo("<center>Le prénom est: $Prenom</center><br><br>");
+  echo("<center>La date est: $Email</center><br><br>"); 
+  echo("<center>Le lieu de naissance est: $DateNaissance</center><br><br>");
+  echo("<center>L'adresse postale est: $AdressePostale</center><br><br>");   
+  echo("<center>Le code postal est: $NumSecu</center><br><br>"); 
+  echo("<center>L'email est: $DateConsul</center><br><br>"); 
+  echo("<center>Le site est: $HeureConsul</center><br><br>"); 
+  echo("<center>Le numéro de téléphone est: $ButConsul</center><br><br>"); 
+  echo("<center>Le semestre est: $NomService</center><br><br>"); 
+  
+  
+  echo("<center>Les connaissances sont:</center><br>");
+  foreach((array)$connaissances as $valeur){
+  echo("<center>$valeur</center><br>");
+  }
+  ?>
+  <hr>
+  
+  </body>
+  
+</html>
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status 
