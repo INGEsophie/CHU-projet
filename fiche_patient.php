7095f8ea4d8dfb07a611fb64f0ec0c7bd07879e5
Sophie INGE <sophie.nath.inge@gmail.com>
2017-03-08 10:06:18 +0100

    Corrections

--- fiche_patient.php
+++ fiche_patient.php
@@ -1,84 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CHU Evreux - Fiche patient</title>
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 
-		<!-- Website CSS style -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
 
-		<!-- Website Font style -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-		<link rel="stylesheet" href="style.css">
-		<!-- Google Fonts -->
-		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
-		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+	<body>
 
-		<title>Fiche patient</title>
-	</head>
-	<body class="bodyh">
+  <?php 
+    include("header.html"); 
+  ?>
 
-
-
-    <div class="container">
-    	<div class="row">
-    		<h2>Patients enregistrés</h2><br>
-    <div class="bs-docs-example">
-
-      <div class="container">
-  	<div class="row">
-
-          <div class="span4">
-          <form class="form-search">
-              <div class="input-append">
-                  <input type="text" class="span2">
-                  <button type="submit" class="btn">Search</button>
-              </div>
-          </form>
-          </div>
-      </div>
-  </div>
-
-
-    	</div>
-    </div>
-
-
-    <div class="row">
-      <h2>Recherche consultations </h2><br>
-  <div class="recherche">
-
-    <div class="container">
-	<div class="row">
-
-        <div class="span4" >
+  <div class="container-fluid">
+    <h1>Outil recherche</h1>
+    <div class="row formulaire">
+    	<legend>Patients enregistrés</legend><br>
         <form class="form-search">
-            <div class="input-append">
-                <input type="text" class="span2">
-                <button type="submit" class="btn">Search</button>
-            </div>
-        </form>
-        </div>
-    </div>
-</div>
+          <input type="text">
+          <button type="submit" class="btn">Ok</button>
+        </form><br>
 
-    <div class="dateform"><p>Date</p>
- <input type="text" id="date" name="date" class="form-control" placeholder="date" required>
+      <legend>Recherche consultations </legend><br>
+        <form class="form-search">
+          <input type="text">
+          <button type="submit" class="btn">OK</button>
+        </form><br>
 
-                        <br><br>
-  <button type="submit" class="btn btn-primary" id="submit">OK</button>
-</div>
+      <legend>Recherche par date</legend><br>
+      <input type="text" id="date" name="date" placeholder="date" required>
+      <button type="submit" class="btn" id="submit">OK</button>
     </div>
   </div>
 
+  <?php 
+    include("footer.html");
+  ?>
 
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.js" />
-
-
-
-		 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
 	</body>
+
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.js" />
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+
 </html>
